621ade2fa74ad0817af722a73f03ce812d49505e
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-06-26 11:14:22 -0400

    Complete

--- src/main/java/EventTracker.java
+++ src/main/java/EventTracker.java
@@ -1,28 +1,44 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class EventTracker implements Tracker {
 
     private static EventTracker INSTANCE = new EventTracker();
 
-    private Map<String, Integer> tracker;
+    protected Map<String, Integer> tracker;
 
     private EventTracker() {
         this.tracker = new HashMap<>();
     }
 
     synchronized public static EventTracker getInstance() {
-        return null;
+        return INSTANCE;
     }
 
     synchronized public void push(String message) {
+        if(tracker.containsKey(message)){
+            Integer toReplace = tracker.get(message);
+            tracker.replace(message, toReplace+1);
+        }
+        else{tracker.put(message, 1);}
     }
 
     synchronized public Boolean has(String message) {
-        return null;
+        if(tracker.containsKey(message) && tracker.get(message) > 0){
+            return true;
+        }
+
+
+        return false;
     }
 
     synchronized public void handle(String message, EventHandler e) {
+        e.handle();
+
+        if(this.has(message)){
+            Integer toReplace = tracker.get(message);
+            tracker.replace(message, toReplace-1);}
     }
 
     // Do not use this. This constructor is for tests only
@@ -30,4 +46,6 @@ public class EventTracker implements Tracker {
     EventTracker(Map<String, Integer> tracker) {
         this.tracker = tracker;
     }
+
+
 }
